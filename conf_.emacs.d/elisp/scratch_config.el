7f3eb9407f8c6675ee18effa51c325e337cd46df
Xander Masotto <xmasotto@gmail.com>
2014-04-29 22:04:19 -0500

    more

--- conf_.emacs.d/elisp/scratch_config.el
+++ conf_.emacs.d/elisp/scratch_config.el
@@ -2,6 +2,6 @@
   (interactive)
   (switch-to-buffer "*scratch*"))
 
-(global-set-key (kbd "M-`") 'switch-scratch)
+(global-set-key (kbd "C-`") 'switch-scratch)
 (setq initial-scratch-message "")
 (setq initial-major-mode 'text-mode)
